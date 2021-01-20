fa6fe21e2b852b311a8a5f5a233b065f6d055744
Rezuan98 <rezuanahmmeds@gmail.com>
2021-01-21 01:06:26 +0600

    added a new methode called display

--- hello.java
+++ hello.java
@@ -3,4 +3,8 @@ public class hello{
     public static void main(string[] arges){
         system.out.println("vs code is awsome")
     }
+    greeting("rezuan ahmmed");
+}
+public static void greeting(string name){
+    system.out.println("wellcome" + name)
 }
